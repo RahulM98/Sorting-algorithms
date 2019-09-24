2649102f292e3aecd74d6eb5067d0ce54869cf09
Rahul Manna <rahulmanna.001@gmail.com>
2019-09-24 22:32:27 +0530

    Update Quick.c

--- Quick.c
+++ Quick.c
@@ -4,14 +4,9 @@
 
 void display(int arr[],int n)
 {
-	//int n = sizeof(arr)/sizeof(arr[0]);
 	int i;
-	//printf("%d\n",(int)sizeof(arr));
-	//printf("%d\n",sizeof(arr[0]));
 	for(i=0;i<n;i++)
-	{
 		printf("%d ",arr[i]);
-	}
 	printf("\n\n");
 }
 
@@ -55,7 +50,6 @@ void QuickSort(int arr[],int left, int right)
 
 int main()
 {
-	// type your code
 	int arr[]={2,6,8,3,4,0,9,1};
 	printf("Before sorting:-\n");
 	display(arr,8);
